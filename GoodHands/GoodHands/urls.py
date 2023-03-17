229d643ccdbb50b9a70cbcd5b9c40193e7b5c292
Kostrvba <kostrubiec50@gmail.com>
2023-03-17 18:42:39 +0100

    changes in htmls with extends and include

--- GoodHands/GoodHands/urls.py
+++ GoodHands/GoodHands/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from GoodHands.IGH import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.LandingPage.as_view(), name="main"),
+    path('donation/', views.AddDonation.as_view(), name="donation"),
+    path('login/', views.Login.as_view(), name="login"),
+    path('register/', views.Register.as_view(), name="register"),
 ]
