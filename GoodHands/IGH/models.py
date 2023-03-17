325842f8fb3617e2c01de0e3d6a6115b4cc7b47e
Kostrvba <kostrubiec50@gmail.com>
2023-03-17 19:09:47 +0100

    models

--- GoodHands/IGH/models.py
+++ GoodHands/IGH/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    FUNDACJA = 'Fundacja'
+    ORGANIZACJA_POZARZADOWA = 'Organizacja pozarządowa'
+    ZBIORKA_LOKALNA = 'Zbiórka lokalna'
+    CHOICES = (
+        (FUNDACJA, 'Fundacja'),
+        (ORGANIZACJA_POZARZADOWA, 'Organizacja pozarządowa'),
+        (ZBIORKA_LOKALNA, 'Zbiórka lokalna'),
+    )
+    type = models.CharField(max_length=50, choices=CHOICES, default=FUNDACJA)
+    categories = models.ManyToManyField('Category')
+
+
+class Donation(models.Model):
+    quantity = models.PositiveIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    phone_number = models.IntegerField(max_length=11)
+    city = models.CharField(max_length=60)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
